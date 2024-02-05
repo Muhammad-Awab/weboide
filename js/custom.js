157f9415fdd2baaa6e18631c3afd54be86577046
Muhammad-Awab <muhammadawab82002@gmail.com>
2024-02-05 20:21:18 +0500

    in Header Section Heading height make small

--- js/custom.js
+++ js/custom.js
@@ -69,7 +69,7 @@ $(document).ready(function() {
       nav:true,
       margin:0,
       autoplay:true,
-      autoplayTimeout:6000,
+      autoplayTimeout:7000,
       autoplayHoverPause:false,
       responsive:{
          0:{
